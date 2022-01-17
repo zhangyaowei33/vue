29930a7fdea0adbc9966027ad15c87d43af0836b
zhangyaowei <1107127347@qq.com>
2022-01-18 01:04:02 +0800

    前端vue新增【登录】功能

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -2,7 +2,7 @@
   <div>
     <el-menu
         style="width: 200px; min-height: calc(100vh - 50px)"
-        default-active="2"
+        default-active="user"
         default-openeds="[1]"
         class="el-menu-vertical-demo"
         >
@@ -13,6 +13,9 @@
       <el-sub-menu index="1">
         <template #title>系统管理</template>
         <el-menu-item index="user">用户管理</el-menu-item>
+      </el-sub-menu>
+      <el-sub-menu index="2">
+        <template #title>价格管理</template>
       </el-sub-menu>
 
     </el-menu>
---
+++ src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div style="width: 100%; height: 100vh; background-color:mediumaquamarine ; overflow: hidden" >
+    <div style="width: 400px; margin: 150px auto">
+      <div style="color: coral;font-size: 40px; text-align: center ;padding: 30px 0">欢迎登录</div>
+      <el-form ref="form" :model="form" size="normal">
+        <el-form-item>
+          <el-input v-model="form.username"></el-input>
+
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width: 100%" type="primary" @click="login">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import request from "@/utils/request";
+import {ElMessage} from "element-plus";
+
+export default {
+  name: "Login.vue",
+  data(){
+    return{
+      form:{}
+    }
+  },
+  methods:{
+    login(){
+        request.post("/user/login", this.form).then(res => {
+          if(res.code === '0'){
+            ElMessage({
+              type: "success",
+              message: "登录成功"
+            })
+            this.$router.push("/") //登录成功后页面自动跳转
+          }else {
+            ElMessage({
+              type: "error",
+              message: res.msg
+            })
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
