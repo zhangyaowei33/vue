1eec8db61efd948f3bee4db58421af492c7c840b
zhangyaowei <1107127347@qq.com>
2022-01-19 01:35:24 +0800

    前端vue新增【注册】功能

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: () =>import("@/views/Login")
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () =>import("@/views/Register")
   }
 ]
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,16 +1,19 @@
 <template>
   <div style="width: 100%; height: 100vh; background-color:mediumaquamarine ; overflow: hidden" >
     <div style="width: 400px; margin: 150px auto">
-      <div style="color: coral;font-size: 40px; text-align: center ;padding: 30px 0">欢迎登录</div>
-      <el-form ref="form" :model="form" size="normal" :rules="rules">
+      <div style="color: coral;font-size: 40px; text-align: center ;padding: 30px 0">欢迎注册</div>
+      <el-form ref="form" :model="form"  size="normal" :rules="rules">
         <el-form-item prop="username">
           <el-input v-model="form.username"></el-input>
         </el-form-item>
         <el-form-item prop="password">
           <el-input v-model="form.password" show-password></el-input>
         </el-form-item>
+        <el-form-item prop="confirm">
+          <el-input v-model="form.confirm" show-password></el-input>
+        </el-form-item>
         <el-form-item>
-          <el-button style="width: 100%" type="primary" @click="login">登 录</el-button>
+          <el-button style="width: 100%" type="primary" @click="register">注 册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -25,11 +28,14 @@ import {reactive} from "vue";
 
 const rules = reactive({
   username: [{required: true, message: '请输入用户名', trigger: 'blur',}],
-  password: [{required: true, message: '请输入密码', trigger: 'blur',}]
+  password: [{required: true, message: '请输入密码', trigger: 'blur',}],
+  confirm: [{required: true, message: '请确认密码', trigger: 'blur',}]
 })
 
+
+
 export default {
-  name: "Login.vue",
+  name: "Register.vue",
   data(){
     return{
       form:{},
@@ -37,16 +43,23 @@ export default {
     }
   },
   methods:{
-    login(){
-      this.$refs['form'].validate((valid) =>{
-        if(valid){
-          request.post("/user/login", this.form).then(res => {
+    register(){
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          if(this.form.password !== this.form.confirm){
+            ElMessage({
+              type: "error",
+              message: "两次输入的密码不一致"
+            })
+            return
+          }
+          request.post("/user/register", this.form).then(res => {
             if(res.code === '0'){
               ElMessage({
                 type: "success",
-                message: "登录成功"
+                message: "注册成功"
               })
-              this.$router.push("/") //登录成功后页面自动跳转
+              this.$router.push("/login") //注册成功后页面自动跳转
             }else {
               ElMessage({
                 type: "error",
@@ -56,12 +69,7 @@ export default {
           })
         }
       })
-
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
